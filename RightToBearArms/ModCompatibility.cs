a5fa2f550b53e9ebcb935a701ad5d1c92efab73e
lammas123 <48428621+lammas321@users.noreply.github.com>
2025-12-12 03:02:04 -0600

    Update v1.1.0

---
+++ RightToBearArms/ModCompatibility.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx.IL2CPP;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace RightToBearArms
+{
+    internal static class ChatCommandsCompatibility
+    {
+        internal static bool? enabled;
+        internal static bool Enabled { get => enabled == null ? (bool)(enabled = IL2CPPChainloader.Instance.Plugins.ContainsKey("lammas123.ChatCommands")) : enabled.Value; }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        internal static void RegisterCommand(Type commandType)
+        {
+            IL2CPPChainloader.Instance.Plugins["lammas123.ChatCommands"].Instance.GetType()
+                .Assembly
+                .GetType("ChatCommands.Api")
+                .GetMethod("RegisterCommand", BindingFlags.Static | BindingFlags.Public)
+                .Invoke(null, [Activator.CreateInstance(commandType)]);
+        }
+    }
+}
--- RightToBearArms/Plugin.cs
+++ RightToBearArms/Plugin.cs
@@ -1,12 +1,14 @@
 using BepInEx;
 using BepInEx.IL2CPP;
 using HarmonyLib;
+using System;
 using System.Globalization;
+using System.Reflection;
 
 namespace RightToBearArms
 {
     [BepInPlugin($"lammas123.{MyPluginInfo.PLUGIN_NAME}", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-    [BepInDependency("lammas123.ChatCommands")]
+    [BepInDependency("lammas123.ChatCommands", BepInDependency.DependencyFlags.SoftDependency)]
     public class RightToBearArms : BasePlugin
     {
         public override void Load()
@@ -16,8 +18,16 @@ namespace RightToBearArms
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
             CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
-            ChatCommands.Api.RegisterCommand(new ItemsCommand());
-            ChatCommands.Api.RegisterCommand(new GiveCommand());
+            if (ChatCommandsCompatibility.Enabled)
+            {
+                Assembly asm = typeof(RightToBearArms).Assembly;
+                
+                Type itemsCommandType = asm.GetType("RightToBearArms.ItemsCommand");
+                Type giveCommandType = asm.GetType("RightToBearArms.GiveCommand");
+
+                ChatCommandsCompatibility.RegisterCommand(itemsCommandType);
+                ChatCommandsCompatibility.RegisterCommand(giveCommandType);
+            }
             
             Harmony.CreateAndPatchAll(typeof(Patches));
             Log.LogInfo($"Loaded [{MyPluginInfo.PLUGIN_NAME} {MyPluginInfo.PLUGIN_VERSION}]");
